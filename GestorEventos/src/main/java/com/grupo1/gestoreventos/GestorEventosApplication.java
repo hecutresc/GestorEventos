84ec79474f7867170e57082d1ad7ea3cddc32cd7
Guillermo <100943009+Guimar08@users.noreply.github.com>
2023-01-16 12:04:43 +0100

    Commit inicial
    Estructura del proyecto vacía (Sin implementación)

---
+++ GestorEventos/src/main/java/com/grupo1/gestoreventos/GestorEventosApplication.java
@@ -0,0 +1,13 @@
+package com.grupo1.gestoreventos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestorEventosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestorEventosApplication.class, args);
+	}
+
+}
---
+++ GestorEventos/src/test/java/com/grupo1/gestoreventos/GestorEventosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupo1.gestoreventos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestorEventosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
