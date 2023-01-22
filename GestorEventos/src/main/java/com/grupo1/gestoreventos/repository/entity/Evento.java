bc24eba4309da82667bb84946332686bdea51f50
jbrunet <jbrunet@PC-JAVI>
2023-01-22 19:40:07 +0100

    Eliminadas las clases Salon y creación entidades Invitado y Evento.

---
+++ GestorEventos/src/main/java/com/grupo1/gestoreventos/repository/entity/Evento.java
@@ -0,0 +1,14 @@
+package com.grupo1.gestoreventos.repository.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "eventos")
+public class Evento {
+
+	
+	
+}
---
+++ GestorEventos/src/main/java/com/grupo1/gestoreventos/repository/entity/Invitado.java
@@ -0,0 +1,12 @@
+package com.grupo1.gestoreventos.repository.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "invitados")
+public class Invitado {
+
+}
