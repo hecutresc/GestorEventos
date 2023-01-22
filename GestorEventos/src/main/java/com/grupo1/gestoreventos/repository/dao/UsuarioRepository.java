0f09cbb79b5cd53979b52e5eb784da79443b664c
Guillermo <guimarros@alu.edu.gva.es>
2023-01-22 12:10:59 +0100

    Implementación de las clases Repository y Service

--- GestorEventos/src/main/java/com/grupo1/gestoreventos/repository/dao/UsuarioRepository.java
+++ GestorEventos/src/main/java/com/grupo1/gestoreventos/repository/dao/UsuarioRepository.java
@@ -2,11 +2,11 @@ package com.grupo1.gestoreventos.repository.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.grupo1.gestoreventos.repository.entity.Direccion;
+import com.grupo1.gestoreventos.repository.entity.Usuario;
 
 import jakarta.transaction.Transactional;
 
 @Transactional
-public interface DireccionRepository extends JpaRepository<Direccion, Long>{
+public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
 	
 }
