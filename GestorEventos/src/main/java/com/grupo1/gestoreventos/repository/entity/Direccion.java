0f055185936aeb6365f8e44c3ed9126fa59f830c
Guillermo <guimarros@alu.edu.gva.es>
2023-01-24 13:19:33 +0100

    Direccion entity a main

--- GestorEventos/src/main/java/com/grupo1/gestoreventos/repository/entity/Direccion.java
+++ GestorEventos/src/main/java/com/grupo1/gestoreventos/repository/entity/Direccion.java
@@ -1,5 +1,32 @@
 package com.grupo1.gestoreventos.repository.entity;
 
-public class Direccion{
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "direcciones")
+public class Direccion {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(name = "calle")
+	private String calle;
+    
+	@Column(name = "numero")
+	private String numero;
+	
+	@Column(name = "ciudad")
+	private String ciudad;
+	
+	@Column(name = "cp")
+	private String cp;
 	
 }
