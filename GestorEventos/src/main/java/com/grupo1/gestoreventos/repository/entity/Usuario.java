eab8ad68189609ba630c6cb8562e5b4a42ac17da
Guillermo <100943009+Guimar08@users.noreply.github.com>
2023-01-24 13:11:55 +0100

    Update Usuario.java

--- GestorEventos/src/main/java/com/grupo1/gestoreventos/repository/entity/Usuario.java
+++ GestorEventos/src/main/java/com/grupo1/gestoreventos/repository/entity/Usuario.java
@@ -1,5 +1,12 @@
-package com.grupo1.gestoreventos.repository.entity;
+package com.grupo1.gestoreventos.repository.dao;
 
-public class Usuario {
+import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.grupo1.gestoreventos.repository.entity.Usuario;
+
+import jakarta.transaction.Transactional;
+
+@Transactional
+public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
+	
 }
