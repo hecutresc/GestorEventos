178c7c4687fabdf202b6742f152ddf80407e8e48
Guillermo <100943009+Guimar08@users.noreply.github.com>
2023-01-24 13:05:39 +0100

    Update UsuarioService.java

--- GestorEventos/src/main/java/com/grupo1/gestoreventos/service/UsuarioService.java
+++ GestorEventos/src/main/java/com/grupo1/gestoreventos/service/UsuarioService.java
@@ -1,5 +1,14 @@
 package com.grupo1.gestoreventos.service;
 
+import java.util.List;
+
+import com.grupo1.gestoreventos.model.dto.UsuarioDTO;
+
 public interface UsuarioService {
+
+	List<UsuarioDTO> findAll();
+	UsuarioDTO findById(UsuarioDTO UsuarioDTO);
+	void save(UsuarioDTO UsuarioDTO);
+	void delete(UsuarioDTO UsuarioDTO);
 
 }
