d1242b57409e35ed1765394130bee39a7e482886
Guillermo <100943009+Guimar08@users.noreply.github.com>
2023-01-24 13:12:22 +0100

    Update Usuario.java

--- GestorEventos/src/main/java/com/grupo1/gestoreventos/repository/entity/Usuario.java
+++ GestorEventos/src/main/java/com/grupo1/gestoreventos/repository/entity/Usuario.java
@@ -1,12 +1,53 @@
-package com.grupo1.gestoreventos.repository.dao;
+package com.grupo1.gestoreventos.repository.entity;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.Data;
 
-import com.grupo1.gestoreventos.repository.entity.Usuario;
+@Data
+@Entity
+@Table(name = "usuarios")
+public class Usuario {
 
-import jakarta.transaction.Transactional;
-
-@Transactional
-public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 	
+	@Column(name = "tipo")
+	private int tipo;
+	
+	@Column(name = "nombre")
+	private String nombre;
+	
+	@Column(name = "apeliidos")
+	private String apellidos;
+	
+	@Column(name = "nif")
+	private String nif;
+	
+	@Column(name = "telefono")
+	private String telefono;
+	
+	@Column(name = "email")
+	private String email;
+	
+	@Column(name = "nombre_usuario")
+	private String nombreUsuario;
+	
+	@Column(name = "contrasena")
+	private String contrasena;
+
+	@Column(name = "activo")
+	private int activo;
+
+	@OneToMany
+	@JoinColumn(name = "id_direccion")
+	private Direccion direccion;
+
 }
