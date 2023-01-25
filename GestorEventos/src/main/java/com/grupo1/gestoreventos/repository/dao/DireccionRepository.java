e45adaf5b9dbb4687d03080b39f28cc6f762da3c
jbrunet <jbrunet@DESKTOP-QCCISJO>
2023-01-25 10:53:28 +0100

    Comiteo diario pal hector

--- GestorEventos/src/main/java/com/grupo1/gestoreventos/repository/dao/DireccionRepository.java
+++ GestorEventos/src/main/java/com/grupo1/gestoreventos/repository/dao/DireccionRepository.java
@@ -1,9 +1,8 @@
 package com.grupo1.gestoreventos.repository.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import org.springframework.stereotype.Repository;
 import com.grupo1.gestoreventos.repository.entity.Direccion;
-
 import jakarta.transaction.Transactional;
 
 @Transactional
